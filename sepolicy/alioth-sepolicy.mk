53db86e473a75765d120e438611fc46baa59725a
Alec Chan <alec3d02@gmail.com>
2024-03-26 21:56:42 +0800

    alioth: sepolicy: label vold_prepare_subdirs
    avc:  denied  { relabelfrom } for  name="checkin" dev="dm-38" ino=18661 scontext=u:r:vold_prepare_subdirs:s0 tcontext=u:object_r:checkin_data_file:s0 tclass=dir permissive=0
    
    Signed-off-by: peternmuller <pm563838@gmail.com>

--- sepolicy/alioth-sepolicy.mk
+++ sepolicy/alioth-sepolicy.mk
@@ -44,3 +44,4 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     device/xiaomi/alioth/sepolicy/private/xiaomi/display \
     device/xiaomi/alioth/sepolicy/private/xiaomi/hardware \
     device/xiaomi/alioth/sepolicy/private/xiaomi/sensors \
+    device/xiaomi/alioth/sepolicy/private/xiaomi/vold
